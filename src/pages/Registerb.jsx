7f2c8a6a2f0907b58e85229637e02488aab3f315
Hojoong-Rome <hojoong9919@naver.com>
2023-11-30 15:43:41 +0900

    wow

--- src/pages/Registerb.jsx
+++ src/pages/Registerb.jsx
@@ -13,11 +13,12 @@ import ButtonType from '../components/ButtonType'
 const Registerb = () => {
   const [nick, setNick] = useState();
   const [userIcon, setUserIcon] = useState();
+  const [userImg, setUserImg] = useState();
   const [fileName, setFileName] = useState();
-  const [ show, setShow ] = useState(false);
 
+  const navigate = useNavigate();
   const user = auth.currentUser;
-
+  
   const encodeFileToBase64 = (fileB) => {
      const reader = new FileReader();
      reader.readAsDataURL(fileB);
@@ -29,13 +30,50 @@ const Registerb = () => {
      })
   }
 
+    //파일확장자 추출함수
+    const extExt = (filename) => {
+      //마지막에 있는 점의 위치
+      const lastDot = filename.lastIndexOf(".");
+      //subString 함수로 마지막 부분만 추출 , 모두 소문자로 변환
+      return filename.substring(lastDot, filename.length).toLowerCase();
+   }
+
   const imgChange = (e) => {
      const newIcon = e.target.files[0];
      encodeFileToBase64(newIcon);
      setUserIcon(newIcon);
      setFileName(newIcon.name);
+     setUserImg(e.target.files[0]);
+     console.log(extExt(fileName));
   }
-  console.log(user);
+
+  const onPress = async (e) => {
+     e.preventDefault();
+     let photoURL = '';
+     if(fileName !== null){
+         try{
+            //확장자 추출
+            const fileExt = extExt(fileName);
+            const imgRef = ref(storage, `userdata/${v4()}${fileExt}`);
+            await uploadBytes(imgRef, userImg);
+            photoURL = await getDownloadURL(imgRef);
+         }catch(err){
+            console.error('Error image upload :' , err);
+         }  
+      } //if
+      try{
+         await updateProfile(
+            user, {
+               displayName: nick,
+               photoURL
+            }
+         )
+      }catch(err){
+         console.error('Error user update profile :', err);
+      }
+      navigate('/');
+  }
+
   return (
         <>
         <div className="loginContainer">
@@ -52,7 +90,7 @@ const Registerb = () => {
           </div>
           <h1 className="text-center">MyChat 회원가입</h1>
           <p>2단계 : 닉네임(필수), 이미지아이콘(선택) 등록</p>
-          <form className="loginForm">
+          <form className="loginForm" method="post">
           <InputType types="text"
                      names="nick"
                      values={nick}
@@ -78,8 +116,8 @@ const Registerb = () => {
           <ButtonType types="submit"
                       classNames="button"
                       styles={{backgroundColor:"green"}}
-                      functions=""
-                      text="다음단계"
+                      functions={onPress}
+                      text="회원가입완료"
           />          
           </form>
          </div>
